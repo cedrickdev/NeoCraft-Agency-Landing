eef654e877f186d57e3489ffbfd7bc95c16b3f93
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-07-18 19:43:30 +0200

    feat(sitemap): update sitemap configuration and enhance robots.txt
    Added new options to the sitemap configuration, including sitemapSize, changefreq, and priority for better SEO management. Updated robots.txt to specify the host and improve search engine indexing.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://neocraftdev.vercel.app", // Remplace par l’URL de ton site en production
-  generateRobotsTxt: true, // (optionnel) Génère aussi un robots.txt
-  // ...d’autres options si besoin
+  siteUrl: "https://neocraftdev.vercel.app",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.7,
 };
 
