ea6f5a559f0151159339301d7eb3d33f1141161a
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-08-13 00:48:20 +0200

    feat(layout): add Facebook Pixel tracking code
    Integrated Facebook Pixel tracking code into the layout component to enhance user activity tracking. This addition complements the existing Plausible tracking, providing a more comprehensive analytics solution.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -152,6 +152,32 @@ export default async function LocaleLayout({ children, params }: Props) {
           data-domain="neocraft.dev"
           src="https://plausible.io/js/script.pageview-props.tagged-events.js"
         ></script>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1139222559815185');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt="Facebook Pixel"
+            src="https://www.facebook.com/tr?id=1139222559815185&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className={inter.className}>
         <ThemeProvider>
